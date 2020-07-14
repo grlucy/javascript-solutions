c74b4f6bdf3ba3111d13a104371d93ad6a112ebb
unknown <53538316+grlucy@users.noreply.github.com>
2020-07-14 12:25:18 -0400

    solved LeetCode algorithm Decompress Run Length Encoded List

---
+++ LeetCode/Decompress-Run-Length-Encoded-List/decompress.js
@@ -0,0 +1,9 @@
+var decompressRLElist = function (nums) {
+  let result = [];
+  for (let i = 0; i < nums.length; i += 2) {
+    for (let j = 1; j <= nums[i]; j++) {
+      result.push(nums[i + 1]);
+    }
+  }
+  return result;
+};
